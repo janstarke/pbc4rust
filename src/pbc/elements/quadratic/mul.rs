ba5598a17a6f9329010e09142e801af5d520f667
Jan Starke <jan.starke@outofbed.org>
2022-01-03 21:30:49 +0100

    the naive multiplication doesn't work, either

--- src/pbc/elements/quadratic/mul.rs
+++ src/pbc/elements/quadratic/mul.rs
@@ -4,7 +4,21 @@ use std::ops::Mul;
 macro_rules! implement_mul {
     ($lhs:tt, $rhs:tt) => {
         {
+            let nqr = F::nqr($lhs.field().target_field());
+            /* naive */
+            let e0 = $lhs.x.clone() * &$rhs.x;
+            let e1 = ($lhs.y.clone() * &$rhs.y);
+            let e1 = e1 * nqr;
+            let e0 = e0 + e1;
+
+            let e1 = $lhs.x.clone() * &$rhs.y;
+            let e2 = $lhs.y.clone() * &$rhs.x;
+            let e1 = e1 + e2;
+
+            let x = e0;
+            let y = e1;
             /* Implementation of the Karatsuba alorithm */
+            /*
             let e0 = $lhs.x.clone() + &$lhs.y;
             let e1 = $rhs.x.clone() + &$rhs.y;
             let e2 = e0.clone() * &e1;
@@ -16,6 +30,7 @@ macro_rules! implement_mul {
 
             let x = (e1.clone() * nqr) + &e0;
             let y = (e2 - e0) - e1;
+            */
 
             Quadratic {x, y, field: $lhs.field()}
         }
